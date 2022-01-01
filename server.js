e05775c10757360d705c2f5f43b9cd35ac130721
pintubordoloi <41015722+pintubordoloi@users.noreply.github.com>
2022-01-01 14:56:48 +0530

    second update

--- server.js
+++ server.js
@@ -6,16 +6,24 @@ const expressLayouts = require('express-ejs-layouts')
 const indexRouter = require('./routes/index')
 const aboutRouter = require('./routes/aboutus')
 const collegedetailsRouter = require('./routes/collegedetails')
+const dotenv = require('dotenv')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+dotenv.config({path:'env'})
 
-/*DATABASE_URL = "mongodb+srv://:hello@dango.0lkqu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+//DATABASE_URL = "mongodb+srv://:hello@dango.0lkqu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
-const mongoose = require('mongoose')
+app.use('/', indexRouter)
+app.use('/aboutus', aboutRouter)
+app.use('/collegedetails', collegedetailsRouter)
+//app.use(bodyParser.urlencode({limit: '10mb', extended: false}))
+
+
+/*const mongoose = require('mongoose')
 mongoose.createConnection(DATABASE_URL, {
     useNewUrlParser: true 
 })
@@ -23,10 +31,6 @@ const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to mongoose'))
 */
-
-app.use('/', indexRouter)
-app.use('/aboutus', aboutRouter)
-app.use('/collegedetails', collegedetailsRouter)
 
 
 app.listen(process.env.PORT || 3000)
